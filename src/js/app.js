dbbd24a0da1279242049095c0923d1957bf64ab2
Annie <overheads165@gmail.com>
2022-12-31 01:01:33 +0100

    linter

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
-const button = document.querySelector("#collapse-button");
-const content = document.querySelector("#collapse-content");
+const button = document.querySelector('#collapse-button');
+const content = document.querySelector('#collapse-content');
 
-button.addEventListener("click", () => {
+button.addEventListener('click', () => {
   if (content.style.maxHeight) {
     content.style.maxHeight = null;
   } else {
-    content.style.maxHeight = content.scrollHeight + "px";
+    content.style.maxHeight = `${content.scrollHeight}px`;
   }
 });
